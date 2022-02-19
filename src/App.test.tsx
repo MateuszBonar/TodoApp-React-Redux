64f5e50ca8daf79f55886252c7e097f391ab8ac3
Mateusz Bonar <mateusz211@vp.pl>
2022-02-19 11:25:58 +0100

    App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,18 +1,26 @@
+//jest.clear all mocks zeby testy dobrze dzialaly jak cos zmieniasz
+
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import { MainPage } from './pages';
+import i18n from './utils/i18n/i18nForTests';
+import { I18nextProvider } from 'react-i18next';
 
 describe('App', () => {
   let store: any;
-
   const mockStore = configureMockStore([thunk]);
 
   jest.mock('i18next', () => ({
     init: () => {},
     t: (k: string) => k,
+  }));
+
+  jest.mock('react-redux', () => ({
+    useSelector: jest.fn(passedFun => passedFun()),
   }));
 
   beforeEach(() => {
@@ -21,11 +29,15 @@ describe('App', () => {
     });
   });
 
-  it('should render correctly', () => {
+  const getWrapper = () =>
     shallow(
       <Provider store={store}>
         <App />
       </Provider>
     );
+
+  it('should render correctly', () => {
+    const wrapper = getWrapper();
+    expect(wrapper.find(MainPage)).toBeTruthy();
   });
 });
